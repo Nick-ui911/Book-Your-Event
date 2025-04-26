26274290534413718f02859552cb9398c767f2d7
Nick-ui911 <nbaghel392@gmail.com>
2025-04-26 15:32:54 +0530

    sixth

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,15 +4,14 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB4yXiZb62DZbVFM8W8UqRKSBPLHEfcNh8",
-  authDomain: "bookevent-d1d71.firebaseapp.com",
-  projectId: "bookevent-d1d71",
-  storageBucket: "bookevent-d1d71.firebasestorage.app",
-  messagingSenderId: "83710566614",
-  appId: "1:83710566614:web:8b8e08ca6e1ca2bf41a977",
-  measurementId: "G-2VVJ7JKL2E"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
-
 
 
 // Initialize Firebase
