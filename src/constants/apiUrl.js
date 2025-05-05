4e474dff85781f6c6d2722de37f7479e54992eab
Nick-ui911 <nbaghel392@gmail.com>
2025-05-05 12:25:03 +0530

    seventeen

--- src/constants/apiUrl.js
+++ src/constants/apiUrl.js
@@ -1,1 +1,4 @@
-export const BASE_URL = "http://localhost:3000/api";
+export const BASE_URL =
+  typeof window !== "undefined" && window.location.hostname === "localhost"
+    ? "http://localhost:3000/api"
+    : "https://book-your-event.vercel.app/api";
